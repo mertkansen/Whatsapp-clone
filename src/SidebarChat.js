b09da83ea8aaa4dd1028eb28a39473a2dae61cd3
Mertkan <mertkansen@gmail.com>
2020-08-20 21:44:26 +0300

    initial point

---
+++ src/SidebarChat.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './SidebarChat.css'
+
+function SidebarChat() {
+
+    return (
+        <div className="sidebarChat">
+            <h1>Sidebar chat</h1>
+        </div>
+    )
+}
+
+export default SidebarChat
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCyLx_OZLEIzwdhULh-PwWqWvaFbKeg5xQ",
+    authDomain: "whatsapp-clone-441be.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-441be.firebaseio.com",
+    projectId: "whatsapp-clone-441be",
+    storageBucket: "whatsapp-clone-441be.appspot.com",
+    messagingSenderId: "363456819517",
+    appId: "1:363456819517:web:a467388ff56f6029b99554",
+    measurementId: "G-8T82K93J4H"
+  };
